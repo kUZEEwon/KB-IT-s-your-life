9fa66afae73236fd2316d843f1f24f340f47ecc9
kUZEEwon <pink7519@pusan.ac.kr>
2024-05-22 16:44:22 +0900

    express수업

---
+++ Node/express/server-1.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+app.get(`/`, (req, res) => {
+    console.log(`/접속성공!`);
+/* 
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.write("<!DOCTYPE html>");
+
+    res.write("<html>");
+
+    res.write("<head>");
+    res.write("<title>응답페이지</title>");
+    res.write("</head>");
+
+    res.write("<body>");
+    res.write("<h1>반가워요, 응답 html 이에요.</h1>");
+    res.write("</body>");
+
+
+    res.write("</head>");
+
+    res.write("</html>"); */
+
+
+    // url을 통해서 data를 전송하고 취득하는 처리를 => Restful 
+    
+    res.end('Hello Express');
+});
+
+const listener = app.listen(3000, () => {
+    console.log(`express server start port : ${listener.address().port}`);
+});
