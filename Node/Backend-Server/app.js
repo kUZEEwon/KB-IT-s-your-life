77877132b09a99fca7a7f617d391dd212d9b2356
kUZEEwon <pink7519@pusan.ac.kr>
2024-05-23 17:23:55 +0900

    express 서버 실습-postman 사용

---
+++ Node/Backend-Server/app.js
@@ -0,0 +1,33 @@
+
+const express = require(`express`);
+const app = express();
+
+let member = [
+    { id:"abc", name:'홍길동', age:24 },
+    { id:"def", name:'랩몬스터', age:30 },
+    { id:"zue", name:'전정국', age:29 }
+]
+
+const server = app.listen(3000, () => {
+    console.log(`server start port: 3000`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.get(`/test`, (req, res) => {
+    // console.log(req.query.id);
+
+    res.send(`안녕하세요`);
+});
+
+app.get(`/check`, (req, res) => {
+    const memberId = req.query.id;
+    const foundMember = member.find(member => member.id === memberId);
+    if (foundMember) {
+        res.send(foundMember);
+    } else {
+        res.send('Member not found');
+    }
+});
